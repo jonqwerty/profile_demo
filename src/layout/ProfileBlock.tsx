30789d24e8e968aeba981ba25ab848605a406f8d
Serhii B <s_blyshchak@ukr.net>
2024-03-07 21:42:38 +0200

    fix gradient

--- src/layout/ProfileBlock.tsx
+++ src/layout/ProfileBlock.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {FC} from 'react';
 
 import ButtonNavigation from '../components/ButtonNavigation';
 import {Colors, FontFamily} from '../common/style';
@@ -10,49 +10,56 @@ import ProfileReviews from '../components/ProfileReviews';
 import StarIcon from '../icons/StarIcon';
 import FollowBlock from '../components/FollowBlock';
 import Button from '../components/Button';
+import {LinearGradient} from 'react-native-linear-gradient';
 
-const ProfileBlock = () => {
+const ProfileBlock: FC = () => {
   return (
-    <View>
-      <ButtonNavigation
-        bgColor={Colors.white}
-        icon={<LeftArrow fill={Colors.black} width={15} height={18} />}
-      />
-      <Avatar isChecked={true} />
-      <ButtonNavigation
-        bgColor={Colors.white}
-        icon={<DotsIcon fill={Colors.black} width={16} height={4} />}
-        right={0}
-      />
-      <Text style={styles.name}>Kate Jamezi-Liamson</Text>
-      <View style={styles.followBlock}>
-        <ProfileReviews
-          icon={<StarIcon fill={Colors.black} width={20} height={20} />}
-          numberOne="4.98"
-          numberTwo="402"
-          text="reviews"
+    <LinearGradient
+      colors={[Colors.lime, Colors.white]}
+      start={{x: 0, y: 0}}
+      end={{x: 0, y: 0.4}}>
+      <View style={styles.wrapper}>
+        <ButtonNavigation
+          bgColor={Colors.white}
+          icon={<LeftArrow fill={Colors.black} width={15} height={18} />}
         />
-      </View>
-      <View style={styles.buttonsBlock}>
-        <Button name={'Following'} />
-        <Button name={'Message'} />
-      </View>
-      <View style={styles.followBlock}>
-        <FollowBlock number={'555'} text={'followers'} />
-        <FollowBlock number={'113'} text={'following'} marginLeft={15} />
-      </View>
+        <Avatar isChecked={true} />
+        <ButtonNavigation
+          bgColor={Colors.white}
+          icon={<DotsIcon fill={Colors.black} width={16} height={4} />}
+          right={0}
+        />
+        <Text style={styles.name}>Kate Jamezi-Liamson</Text>
+        <View style={styles.followBlock}>
+          <ProfileReviews
+            icon={<StarIcon fill={Colors.black} width={20} height={20} />}
+            numberOne="4.98"
+            numberTwo="402"
+            text="reviews"
+          />
+        </View>
+        <View style={styles.buttonsBlock}>
+          <Button name={'Following'} />
+          <Button name={'Message'} />
+        </View>
+        <View style={styles.followBlock}>
+          <FollowBlock number={'555'} text={'followers'} />
+          <FollowBlock number={'113'} text={'following'} marginLeft={15} />
+        </View>
 
-      <Text style={styles.description}>
-        I am an Experience Designer with a deep passion for travel around the
-        world, cars, trains, snowboarding and luxury yachts
-      </Text>
-    </View>
+        <Text style={styles.description}>
+          I am an Experience Designer with a deep passion for travel around the
+          world, cars, trains, snowboarding and luxury yachts
+        </Text>
+      </View>
+    </LinearGradient>
   );
 };
 
 export default ProfileBlock;
 
 const styles = StyleSheet.create({
+  wrapper: {marginHorizontal: 24, marginTop: 12},
   name: {
     marginTop: 24,
     fontFamily: FontFamily.outfit_semibold,
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,6 +1,5 @@
 import {ScrollView, StyleSheet, View} from 'react-native';
 import React, {FC} from 'react';
-import LinearGradient from 'react-native-linear-gradient';
 
 import {Colors} from '../common/style';
 import ProfileBlock from '../layout/ProfileBlock';
@@ -8,17 +7,12 @@ import ProfileBlock from '../layout/ProfileBlock';
 const ProfileScreen: FC = () => {
   return (
     <View style={styles.container}>
-      <LinearGradient
-        colors={[Colors.lime, Colors.white]}
-        start={{x: 0, y: 0}}
-        end={{x: 0, y: 0.7}}>
-        <ScrollView
-          contentContainerStyle={styles.contentContainerStyle}
-          showsVerticalScrollIndicator={false}
-          style={styles.scroll}>
-          <ProfileBlock />
-        </ScrollView>
-      </LinearGradient>
+      <ScrollView
+        contentContainerStyle={styles.contentContainerStyle}
+        showsVerticalScrollIndicator={false}
+        style={styles.scroll}>
+        <ProfileBlock />
+      </ScrollView>
     </View>
   );
 };
@@ -34,5 +28,5 @@ const styles = StyleSheet.create({
   contentContainerStyle: {flexGrow: 1},
 
   wrapper: {height: 100},
-  scroll: {marginHorizontal: 24, marginTop: 12},
+  scroll: {},
 });
